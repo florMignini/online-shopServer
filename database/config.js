98b86d507ab8c03723276443045415acffe74c84
florMignini <flormignini29@gmail.com>
2022-10-24 10:01:15 -0300

    user crud created

--- database/config.js
+++ database/config.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const MONGO_URI = process.env.DB_URI;
+console.log(typeof MONGO_URI);
 export const dbConnection = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
-    console.log(`DB succesfully connected`);
-  } catch (error) {
-    console.log(error);
-  }
+  mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+  mongoose.connection
+    .once("open", function () {
+      console.log("Conection has been made!");
+    })
+    .on("error", function (error) {
+      console.log("Error is: ", error);
+    });
 };
