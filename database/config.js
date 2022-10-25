346453b7d90919f09ae204f8fce41c79d185c2ad
florMignini <flormignini29@gmail.com>
2022-10-25 11:55:22 -0300

    mongodb issues fixed

--- database/config.js
+++ database/config.js
@@ -4,7 +4,11 @@ dotenv.config();
 
 export const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+    await mongoose.connect(
+      (DB_URI =
+        "mongodb+srv://online-shop-user:og5DR7Q1Zej0gx2K@online-shop-server.zplhsya.mongodb.net/?retryWrites=true&w=majority"),
+      { useNewUrlParser: true }
+    );
     console.log(`DB succesfully connected`);
   } catch (error) {
     console.log(error);
